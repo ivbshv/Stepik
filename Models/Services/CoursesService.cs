70bd0b07e417bd8be5f4aad904183fd0c703f952
Bushuev Igor <boommotivator@gmail.com>
2025-05-31 19:12:01 +0300

    add class CoursesService

---
+++ Models/Services/CoursesService.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik.Models.Services
+{
+    public class CoursesService
+    {
+        public static int GetTotalCount()
+        {
+            string connections = Constant.ConnectionString;
+
+            using var connection = new  MySqlConnection(connections);
+
+            connection.Open();
+
+            var query = @"SELECT COUNT(*) FROM courses";
+
+            using var command = new MySqlCommand(query, connection);
+
+            var courseCount = command.ExecuteScalar() ;
+
+            return courseCount != null ?  Convert.ToInt32(courseCount) : 0;
+        }
+    }
+}
